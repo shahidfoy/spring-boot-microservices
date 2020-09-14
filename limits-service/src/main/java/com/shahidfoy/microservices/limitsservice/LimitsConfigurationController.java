b14c5c6167fa614b655f91e2f05a805f9975da55
shahidfoy <shahidfoy@gmail.com>
2020-09-14 16:17:29 -0600

    setup zipkin dashboard and enabled hystrix

--- limits-service/src/main/java/com/shahidfoy/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/shahidfoy/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,17 +1,21 @@
 package com.shahidfoy.microservices.limitsservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.shahidfoy.microservices.limitsservice.bean.LimitConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableHystrix
 public class LimitsConfigurationController {
 
     @Autowired
     private Configuration configuration;
 
     @GetMapping("/limits")
+    @HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
     public LimitConfiguration retrieveLimitsFromConfigurations() {
         return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
     }
